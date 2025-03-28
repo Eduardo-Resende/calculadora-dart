0bc66e39f74f1c86e5a4970577d0d5a1e9a059c8
Eduardo Santana <santana.edu2004@gmail.com>
2025-03-28 09:41:30 -0300

    Adiciona escolha de operação

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -2,9 +2,33 @@ import 'dart:io';
 
 void main() {
   print("Digite o primeiro número: ");
-  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroUm = double.parse(stdin.readLineSync() ?? "0");
   print("Digite o segundo número");
-  double numeroDois = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync() ?? "0");
+  print("Informe a operação [ + - / * ]");
+  String operacao = stdin.readLineSync()!;
+
+  double resultado;
+  switch (operacao) {
+    case "+":
+      resultado = soma(numeroUm, numeroDois);
+      print("A soma $numeroUm + $numeroDois = $resultado");
+      break;
+    case "-":
+      resultado = subtracao(numeroUm, numeroDois);
+      print("A subtração $numeroUm - $numeroDois = $resultado");
+      break;
+     case "/":
+      resultado = divisao(numeroUm, numeroDois);
+      print("A divisão $numeroUm / $numeroDois = $resultado");
+      break;
+    case "*":
+      resultado = multiplicacao(numeroUm, numeroDois);
+      print("A multiplicação $numeroUm * $numeroDois = $resultado");
+      break;   
+    default:
+      print("A operação $operacao é inválida");
+  }
   
 }
 
