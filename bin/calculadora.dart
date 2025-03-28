ea7960e07f3df3d1922cb713366f0d1c184917fa
Eduardo Santana <santana.edu2004@gmail.com>
2025-03-28 10:04:01 -0300

    Melhoria e tratamento de erros

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,35 +1,27 @@
 import 'dart:io';
 
 void main() {
+  double numeroUm;
+  double numeroDois;
+
   print("Digite o primeiro número: ");
-  double numeroUm = double.parse(stdin.readLineSync() ?? "0");
-  print("Digite o segundo número");
-  double numeroDois = double.parse(stdin.readLineSync() ?? "0");
+  try {
+    numeroUm = double.parse(stdin.readLineSync() ?? "0.0");
+  } catch (e) {
+    numeroUm = 0.0;
+  }
+  
   print("Informe a operação [ + - / * ]");
   String operacao = stdin.readLineSync()!;
 
-  double resultado;
-  switch (operacao) {
-    case "+":
-      resultado = soma(numeroUm, numeroDois);
-      print("A soma $numeroUm + $numeroDois = $resultado");
-      break;
-    case "-":
-      resultado = subtracao(numeroUm, numeroDois);
-      print("A subtração $numeroUm - $numeroDois = $resultado");
-      break;
-     case "/":
-      resultado = divisao(numeroUm, numeroDois);
-      print("A divisão $numeroUm / $numeroDois = $resultado");
-      break;
-    case "*":
-      resultado = multiplicacao(numeroUm, numeroDois);
-      print("A multiplicação $numeroUm * $numeroDois = $resultado");
-      break;   
-    default:
-      print("A operação $operacao é inválida");
+  print("Digite o segundo número");
+  try {
+    numeroDois = double.parse(stdin.readLineSync() ?? "0.0");
+  } catch (e) {
+    numeroDois = 0.0;
   }
-  
+
+  calcular(numeroUm, numeroDois, operacao);
 }
 
 double soma(double numeroUm, double numeroDois) => numeroUm + numeroDois;
@@ -39,3 +31,32 @@ double subtracao(double numeroUm, double numeroDois) => numeroUm - numeroDois;
 double divisao(double numeroUm, double numeroDois) => numeroUm / numeroDois;
 
 double multiplicacao(double numeroUm, double numeroDois) => numeroUm * numeroDois;
+
+double calcular(double numeroUm, double numeroDois, String operacao) {
+  double resultado = 0;
+  switch (operacao) {
+    case "+":
+      resultado = soma(numeroUm, numeroDois);
+      print("A soma $numeroUm + $numeroDois = $resultado");
+      break;
+    case "-":
+      resultado = subtracao(numeroUm, numeroDois);
+      print("A subtração $numeroUm - $numeroDois = $resultado");
+      break;
+    case "/":
+      if (numeroDois != 0) {
+        resultado = divisao(numeroUm, numeroDois);
+        print("A divisão $numeroUm / $numeroDois = $resultado");
+      } else {
+        print("Operação inválida, o divisor não pode ser 0");
+      }
+      break;
+    case "*":
+      resultado = multiplicacao(numeroUm, numeroDois);
+      print("A multiplicação $numeroUm * $numeroDois = $resultado");
+      break;
+    default:
+      print("A operação $operacao é inválida");
+  }
+  return resultado;
+}
